2650247740638bc14654af9d4ffe9883978d93f3
Bouchama Lotfi <kayisse@gmail.com>
2022-08-03 23:28:10 +0100

    implimented input maping to entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,6 +11,7 @@ export interface UserAddInput {
   full_name: string;
   email: string;
   age: number;
+  password: string;
 }
 
 export interface UserUpdateInput extends Partial<UserAddInput> {}
@@ -28,14 +29,50 @@ export class UsersService {
 
   add(user: UserAddInput) {}
 
+  getBirthdayFromAge(age: number): Date {
+    var today = new Date();
+    return new Date(today.getUTCFullYear() - age, 0, 1);
+  }
+
   getAgeFromBirthday(birthDate: Date) {
     var today = new Date();
     var age = today.getFullYear() - birthDate.getFullYear();
+
     var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
       age--;
     }
+
     return age;
+  }
+
+  mapUpdateInput(userInput: UserUpdateInput): Partial<UserEntity> {
+    const user: Partial<UserEntity> = {};
+    if ("email" in userInput) {
+      user.email = userInput.email;
+    }
+
+    if ("full_name" in userInput) {
+      const [first_name, last_name] = userInput.full_name.split(" ");
+      user.first_name = first_name;
+      user.last_name = last_name;
+    }
+
+    if ("age" in userInput) {
+      user.birthday = this.ageToDateString(userInput.age);
+    }
+
+    if ("password" in userInput) {
+      user.password = userInput.password;
+    }
+    return user;
+  }
+
+  ageToDateString(age: number) {
+    let birthDate = this.getBirthdayFromAge(age);
+    const offset = birthDate.getTimezoneOffset();
+    birthDate = new Date(birthDate.getTime() - offset * 60 * 1000);
+    return birthDate.toISOString().split("T")[0];
   }
 
   mapEntityToResponse(user: UserEntity): UserResponse {
