81b6c1752bd04955512435812802100767ae30f1
Bouchama Lotfi <kayisse@gmail.com>
2022-08-03 22:39:56 +0100

    implement user maping function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,33 @@
+import { UserEntity, UsersRepository } from "./users.repository";
+
+export interface UserResponse {
+  id: string;
+  full_name: string;
+  email: string;
+  age: number;
+}
+
+export interface UserAddInput {
+  full_name: string;
+  email: string;
+  age: number;
+}
+
+export interface UserUpdateInput extends Partial<UserAddInput> {}
+
 export class UsersService {
+  constructor(private usersRepo: UsersRepository) {}
+
+  getAll() {}
+
+  getById(id: string) {}
+
+  updateById(id: string, user: UserUpdateInput) {}
+
+  deleteById(id: string) {}
+
+  add(user: UserAddInput) {}
+
   getAgeFromBirthday(birthDate: Date) {
     var today = new Date();
     var age = today.getFullYear() - birthDate.getFullYear();
@@ -9,5 +38,14 @@ export class UsersService {
     return age;
   }
 
-  mapUserObject() {}
+  mapEntityToResponse(user: UserEntity): UserResponse {
+    const response: UserResponse = {
+      id: user.id,
+      full_name: user.first_name + " " + user.last_name,
+      email: user.email,
+      age: this.getAgeFromBirthday(new Date(user.birthday)),
+    };
+
+    return response;
+  }
 }
