4799b0378dc585082c6aac1f18223767ac910a7f
Bouchama Lotfi <kayisse@gmail.com>
2022-08-03 22:39:22 +0100

    add user response maping test

--- src/users/users.test.ts
+++ src/users/users.test.ts
@@ -1,15 +1,34 @@
+import { UserEntity, UsersRepository } from "./users.repository";
 import { UsersService } from "./users.service";
 
 describe("users test", () => {
-  const usersService = new UsersService();
+  const userRepo = new UsersRepository();
+  const usersService = new UsersService(userRepo);
 
-  it("should return a user without password and fullname and calculate age instead of birthday", () => {});
+  it("should return a user without password and fullname ", () => {
+    const user: UserEntity = {
+      id: "A1",
+      first_name: "first_nameA1",
+      last_name: "last_nameA1",
+      email: "first_nameA1@gmail.com",
+      password: "passwordA1",
+      birthday: "1992-11-12",
+    };
+    const result = usersService.mapEntityToResponse(user);
+
+    expect(result).toHaveProperty("full_name");
+    expect(result).not.toHaveProperty("password");
+    expect(result).toHaveProperty("age");
+    expect(result.age).toBe(29);
+    expect(result.full_name).toBe("first_nameA1 last_nameA1");
+  });
 
   it("should  return age from birthday", () => {
     const birthDate = new Date(1992, 11, 5);
     expect(usersService.getAgeFromBirthday(birthDate)).toBe(29);
   });
 
+  /*
   it("should return birthday from age", () => {});
 
   it("should verify email uniqueness", () => {});
@@ -17,4 +36,5 @@ describe("users test", () => {
   it("should delete a user", () => {});
 
   it("should return true if user exist by id", () => {});
+  */
 });
