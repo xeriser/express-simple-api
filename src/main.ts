61e4d851646c0fe32f09697cf38289f3d6bc4d4e
Bouchama Lotfi <kayisse@gmail.com>
2022-08-04 01:28:17 +0100

    apply error hundler to the server

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import * as express from "express";
+import { errorHandler } from "./errorHandler";
+import userRoutes from "./users/users.controller";
 
 const app = express();
 
 const port = 8080;
 
 app.use(express.json());
+
+app.use("/api/v1", userRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("Server started");
